1bcf096a6253329e9f7ae66662c8d2b5ba7deff8
benchislett <chislett.ben@gmail.com>
2023-01-16 14:51:36 -0500

    Improved eval collection, faster but one-shot

--- collect_evals.py
+++ collect_evals.py
@@ -1,5 +1,22 @@
 import subprocess
 
+commands = "new game 1\n"
+
+lines=0
 with open("openings.txt", "r") as f:
     for line in f:
-        subprocess.run(['sh', '../extract_evals.sh', line.strip()])
+        if lines > 1000:
+            break
+        lines += 1
+
+        commands += "set board " + line.strip() + "\neval\n"
+
+with open("instructions.txt", "w") as f:
+    f.write(commands)
+
+result = subprocess.run(['gnubg', '-t', '-c', 'instructions.txt'], stdout=subprocess.PIPE)
+result = '\n'.join(filter(lambda s: '2 ply:' in s, result.stdout.decode().split('\n')))
+
+with open("evals.txt", "w") as f:
+    f.write(result)
+

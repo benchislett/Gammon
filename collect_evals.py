3da25929313aa0b4a8350d63b11c0fce054591cc
kipawaa <kipawaa@kipawaa-desktop.localdomain>
2023-01-16 14:01:11 -0500

    updated evals

--- collect_evals.py
+++ collect_evals.py
@@ -2,4 +2,4 @@ import subprocess
 
 with open("openings.txt", "r") as f:
     for line in f:
-        subprocess.run(['zsh', '../extract_evals.sh', line.strip()])
+        subprocess.run(['sh', '../extract_evals.sh', line.strip()])

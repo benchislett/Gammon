05b32c60fc0a9962313caf7e88ea56d848330023
kipawaa <mccubbinri@gmail.com>
2023-01-17 22:16:56 -0500

    PUSH
    q
    git commit -a -m PUSHq

--- collect_evals.py
+++ collect_evals.py
@@ -9,8 +9,17 @@ commandfile = datafile + ".instructions.cmd"
 
 commands = "new game 1\n"
 
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
 lines=0
-MAX_LINES = 9999999999 # 100
+MAX_LINES = 99999999999999 # 100
 with open(datafile, "r") as f:
     for line in f:
         if lines > MAX_LINES:
@@ -22,9 +31,7 @@ with open(datafile, "r") as f:
 with open(commandfile, "w") as f:
     f.write(commands)
 
-result = subprocess.run(['gnubg', '-t', '-c', commandfile], stdout=subprocess.PIPE)
-result = '\n'.join(filter(lambda s: '2 ply:' in s, result.stdout.decode().split('\n')))
-
 with open(outputfile, "w") as f:
-    f.write(result)
-
+    for eval_line in execute(['gnubg', '-t', '-c', commandfile]):
+        if '2 ply:' in eval_line:
+            f.write(eval_line)
--- extract_evals.sh
+++ extract_evals.sh
@@ -1,7 +1,19 @@
 split -l $((`wc -l < $1`/10)) $1 $1.split.txt -da 1
-find . -type f | grep .split.txt | grep -v instructions | parallel "python3 $(git rev-parse --show-toplevel)/collect_evals.py {/} {/}.evals.txt"
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt0 $1_0.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt1 $1_1.evals.txt & 
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt2 $1_2.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt3 $1_3.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt4 $1_4.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt5 $1_5.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt6 $1_6.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt7 $1_7.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt8 $1_8.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_evals.py $1.split.txt9 $1_9.evals.txt &
+
+wait
+
 rm -f evals.txt
 for i in {0..9}
 do
-  cat $1.split.txt$i.evals.txt >> evals.txt
+  cat $1_$i.evals.txt >> evals.txt
 done

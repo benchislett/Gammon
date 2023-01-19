bb26d13c42ab80d271a14bd418ffad8a383329b9
benchislett <chislett.ben@gmail.com>
2023-01-19 12:01:35 -0500

    No swaps in interactive for GNU boards

--- interactive.cpp
+++ interactive.cpp
@@ -12,86 +12,87 @@
 
 int main() {
 
-  Board b(true);
+        Board b(true);
 
-  std::random_device rd;  // obtain a random number from hardware
-  std::mt19937 gen(rd()); // seed the generator
+        std::random_device rd;  // obtain a random number from hardware
+        std::mt19937 gen(rd()); // seed the generator
 
-  std::cout << "New game\n";
+        std::cout << "New game\n";
 
-  auto net = std::make_shared<Net>();
-  torch::load(net, "net.pt");
+        auto net = std::make_shared<Net>();
+        torch::load(net, "net.pt");
 
-  std::string input = "start";
-  int botside       = 0;
+        std::string input = "start";
+        int botside = 0;
 
-  while (input != "quit") {
-    std::cin >> input;
+        while (input != "quit") {
+                std::cin >> input;
 
-    if (input == "swap") {
-      b.swap();
-    }
+                if (input == "swap") {
+                        b.swap();
+                }
 
-    if (input == "eval") {
-      auto board_tensor = load_board(b.encode().serialize()).reshape({1, 200});
-      auto pred_tensor  = net->forward(board_tensor);
+                if (input == "eval") {
+                        auto board_tensor = load_board(b.encode().serialize()).reshape({1, 200});
+                        auto pred_tensor = net->forward(board_tensor);
 
-      std::cout << "Bot eval position " << b.encode().serialize() << " with eval " << pred_tensor[0][0].item<float>()
-                << '\n';
-    }
+                        std::cout << "Bot eval position " << b.encode().serialize() << " with eval "
+                                  << pred_tensor[0][0].item<float>() << '\n';
+                }
 
-    if (input == "botroll") {
-      std::cin >> input;
-      int d0     = input[0] - '0';
-      int d1     = input[1] - '0';
-      auto moves = legal_moves(b, botside, d0, d1);
+                if (input == "botroll") {
+                        std::cin >> input;
+                        int d0 = input[0] - '0';
+                        int d1 = input[1] - '0';
+                        auto moves = legal_moves(b, botside, d0, d1);
 
-      if (moves.size() == 0) {
-        std::cout << "No legal moves\n";
-      } else {
+                        if (moves.size() == 0) {
+                                std::cout << "No legal moves\n";
+                        } else {
 
-        auto before = b.encode().serialize();
+                                auto before = b.encode().serialize();
 
-        std::vector<float> move_evals;
+                                std::vector<float> move_evals;
 
-        for (int move = 0; move < moves.size(); move++) {
-          auto record = b.make_fullmove(botside, moves[move]);
-          b.swap();
+                                for (int move = 0; move < moves.size(); move++) {
+                                        auto record = b.make_fullmove(botside, moves[move]);
 
-          auto board_tensor = load_board(b.encode().serialize()).reshape({1, 200});
-          auto pred_tensor  = net->forward(board_tensor);
+                                        auto board_tensor = load_board(b.encode().serialize()).reshape({1, 200});
+                                        auto pred_tensor = net->forward(board_tensor);
 
-          move_evals.push_back(1.0 - pred_tensor[0][0].item<float>());
+                                        move_evals.push_back(1.0 - pred_tensor[0][0].item<float>());
 
-          std::cout << "Bot eval " << moves[move].serialize() << " with eval " << move_evals.back() << '\n';
+                                        std::cout << "Bot eval " << moves[move].serialize() << " with eval "
+                                                  << move_evals.back() << " (board " << b.encode().serialize() << ")\n";
 
-          b.swap();
-          b.unmake_fullmove(botside, moves[move], record);
+                                        b.unmake_fullmove(botside, moves[move], record);
+                                }
+
+                                int max_index = std::distance(move_evals.begin(),
+                                                              std::max_element(move_evals.begin(), move_evals.end()));
+
+                                b.make_fullmove(botside, moves[max_index]);
+
+                                std::cout << "Bot plays " << moves[max_index].serialize() << " with eval "
+                                          << move_evals[max_index] << '\n';
+                        }
+                }
+
+                if (input == "playermove") {
+                        std::cin >> input;
+                        int from = std::stoi(input.substr(0, input.find('/'))) - 1;
+                        int to = std::stoi(input.substr(input.find('/') + 1)) - 1;
+
+                        b.make_move(1 - botside, from, to);
+                }
+
+                if (input == "board") {
+                        std::cin >> input;
+                        b = Board(EncodedBoard(input));
+                }
+
+                std::cout << b.encode().serialize() << std::endl;
         }
 
-        int max_index = std::distance(move_evals.begin(), std::max_element(move_evals.begin(), move_evals.end()));
-
-        b.make_fullmove(botside, moves[max_index]);
-
-        std::cout << "Bot plays " << moves[max_index].serialize() << " with eval " << move_evals[max_index] << '\n';
-      }
-    }
-
-    if (input == "playermove") {
-      std::cin >> input;
-      int from = std::stoi(input.substr(0, input.find('/'))) - 1;
-      int to   = std::stoi(input.substr(input.find('/') + 1)) - 1;
-
-      b.make_move(1 - botside, from, to);
-    }
-
-    if (input == "board") {
-      std::cin >> input;
-      b = Board(EncodedBoard(input));
-    }
-
-    std::cout << b.encode().serialize() << std::endl;
-  }
-
-  return 0;
+        return 0;
 }

18f1f19c1d624955181b1fca3b7d4ce88caa84c8
benchislett <chislett.ben@gmail.com>
2023-01-30 09:56:27 -0500

    Update rollout collection scripts

--- collect_rollouts.py
+++ collect_rollouts.py
@@ -1,5 +1,48 @@
 import subprocess
+import sys
+import re
 
-with open("games.txt", "r") as f:
+args = sys.argv;
+
+datafile = "openings.txt" if len(args) <= 1 else args[1]
+outputfile = "rollouts.txt" if len(args) <= 2 else args[2]
+commandfile = datafile + ".instructions.cmd"
+
+commands = "new game 1\n"
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+lines=0
+MAX_LINES = 99999999999999 # 100
+with open(datafile, "r") as f:
     for line in f:
-        subprocess.run(['zsh', '../extract_rollout.sh', line.strip()])
+        if lines > MAX_LINES:
+            break
+        lines += 1
+
+        commands += "set board " + line.strip() + "\nrollout\n"
+
+with open(commandfile, "w") as f:
+    f.write(commands)
+
+with open(outputfile, "w") as f:
+    ago = 100
+    for eval_line in execute(['script', '-q', '--log-out', '/dev/null', '-c', 'gnubg -t -c ' + commandfile]):
+        if 'Rollout done.' in eval_line:
+            ago = 0
+        else:
+            ago += 1
+        
+        if ago == 3:
+            f.write(' '.join(map(str, re.findall(r'\d+.\d+', eval_line.strip()))) + '\n')
+            f.flush()
+
+        # if '2 ply:' in eval_line:
+        #     f.write(eval_line)
--- extract_rollout.sh
+++ extract_rollout.sh
@@ -1,3 +1,19 @@
-echo "new game 1\nset board $1\nset turn 0\nrollout" > instructions.txt
-script output.txt -c "gnubg --tty -c instructions.txt"
-grep "Rollout done." output.txt -A 3 | tail -n1 >> scores.txt
+split -l $((`wc -l < $1`/10)) $1 $1.split.txt -da 1
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt0 $1_0.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt1 $1_1.evals.txt & 
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt2 $1_2.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt3 $1_3.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt4 $1_4.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt5 $1_5.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt6 $1_6.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt7 $1_7.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt8 $1_8.evals.txt &
+python3 $(git rev-parse --show-toplevel)/collect_rollouts.py $1.split.txt9 $1_9.evals.txt &
+
+wait
+
+rm -f evals.txt
+for i in {0..9}
+do
+  cat $1_$i.evals.txt >> evals.txt
+done
